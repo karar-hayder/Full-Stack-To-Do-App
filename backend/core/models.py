c79711462b403e71390bc412d0a68a8ee2f3d14d
Karar <krarhayder45@gmail.com>
2024-12-26 02:05:21 +0300

    added todo created and updated attrs

--- backend/core/models.py
+++ backend/core/models.py
@@ -8,5 +8,8 @@ class Todo(models.Model):
     description = models.TextField()
     completed = models.BooleanField(default=False)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title

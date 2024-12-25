2e9a6ef0ed2c03ae29a451b66587bf2256eeb31c
Karar <krarhayder45@gmail.com>
2024-12-26 01:53:12 +0300

    DONE the CRED for the todo

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,1 +1,10 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+from .views import TodoView
+
+router = routers.DefaultRouter()
+router.register(r"todos", TodoView, "todo")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]

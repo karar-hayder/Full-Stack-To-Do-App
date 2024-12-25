2ccf1fb12f3f8432101814ea42e22bb1583d0b5d
Karar <krarhayder45@gmail.com>
2024-12-26 02:19:09 +0300

    DONE user registeration, login and refreash

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,1 +1,10 @@
-urlpatterns = []
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import UserRegistrationView
+
+
+urlpatterns = [
+    path("register/", UserRegistrationView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,29 @@
+from re import A
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 
 # Create your views here.
+
+
+class UserRegistrationView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            access = AccessToken.for_user(user)
+            return Response(
+                {
+                    "user": serializer.data,
+                    "refresh": str(refresh),
+                    "access": str(access),
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

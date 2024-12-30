bf001b347eaf4fae8e7278e1cb9a31526e4290e6
Karar <krarhayder45@gmail.com>
2024-12-31 02:54:07 +0300

    Add login and registration pages with form handling and API integration

---
+++ frontend/src/app/(auth)/register/page.tsx
@@ -0,0 +1,74 @@
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
+export default function Login() {
+  async function handleLogin(formData: FormData): Promise<void> {
+    "use server";
+
+    const username = formData.get("username") as string | null;
+    const password = formData.get("password") as string | null;
+
+    if (!username || !password) {
+      console.error("Username and password are required.");
+      return;
+    }
+
+    try {
+      const apiUrl = process.env.DJANGO_PUBLIC_API_URL;
+
+      if (!apiUrl) {
+        console.error("API URL is not defined in the environment variables.");
+        return;
+      }
+      console.log(`${apiUrl}users/register/`);
+      const response = await fetch(`${apiUrl}users/register/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        console.error("Failed to Reister:", response.statusText);
+        return;
+      }
+
+      const data = await response.json();
+      console.log("Register successful");
+
+      (await cookies()).set("access-token", data.access, { httpOnly: true });
+      (await cookies()).set("refreash-token", data.refreash, {
+        httpOnly: true,
+      });
+    } catch (error) {
+      console.error("An error occurred while logging in:", error);
+    }
+    redirect("/");
+  }
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <form action={handleLogin} className="flex flex-col gap-4 ">
+          <h1 className="text-4xl font-bold">Register</h1>
+          <input
+            name="username"
+            type="text"
+            placeholder="Username"
+            className="p-2 border border-gray-300 rounded"
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="p-2 border border-gray-300 rounded"
+          />
+          <button type="submit" className="p-2 bg-blue-500 text-white rounded">
+            Submit
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+}

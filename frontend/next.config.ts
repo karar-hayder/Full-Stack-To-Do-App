23d7c5baea234415a3db24b5fe6f749e50dd3c34
Karar <krarhayder45@gmail.com>
2024-12-31 02:42:17 +0300

    renamed api env name to combat interference

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+process.loadEnvFile("../.env");
+const apiUrl = process.env.DJANGO_PUBLIC_API_URL;
+console.log(apiUrl);
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    DJANGO_PUBLIC_API_URL: apiUrl,
+  },
 };
 
 export default nextConfig;

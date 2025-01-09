2d006d4038f59643f40bde3ce88da6858179f756
Karar <krarhayder45@gmail.com>
2025-01-09 05:58:04 +0300

    todos frontend APIs

---
+++ frontend/src/lib/todos.ts
@@ -0,0 +1,97 @@
+import { getToken } from "./auth";
+
+const api = process.env.DJANGO_PUBLIC_API_URL;
+
+export const getTodos = async (pageNumber: number = 1) => {
+  const token = await getToken();
+  if (!token) {
+    return false;
+  }
+  const response = await fetch(
+    `${api}core/todos/?page=${pageNumber}&filter_by=completed&filter_by=-created_at`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token.accessToken}`,
+      },
+      credentials: "include",
+      cache: "no-store",
+    }
+  );
+  if (!response.ok) {
+    return response.status;
+  }
+  const data = await response.json();
+  return data;
+};
+
+export const editTodo = async (
+  id: number,
+  title: string,
+  description: string,
+  complete: boolean
+) => {
+  const token = await getToken();
+  if (!token) {
+    return false;
+  }
+  const response = await fetch(`${api}core/todos/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token.accessToken}`,
+    },
+    credentials: "include",
+    body: JSON.stringify({
+      title: title,
+      description: description,
+      completed: complete,
+    }),
+  });
+  return response;
+};
+
+export const deleteTodo = async (id: number) => {
+  const token = await getToken();
+  if (!token) {
+    return false;
+  }
+  const response = await fetch(`${api}core/todos/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    credentials: "include",
+  });
+  return response;
+};
+
+export const createTodo = async (
+  title: string,
+  description: string,
+  complete: boolean
+) => {
+  const token = await getToken();
+  if (!token) {
+    return false;
+  }
+  const response = await fetch(`${api}core/todos/`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token.accessToken}`,
+    },
+    credentials: "include",
+    cache: "no-store",
+    body: JSON.stringify({
+      title: title,
+      description: description,
+      completed: complete,
+    }),
+  });
+  if (!response.ok) {
+    return response.status;
+  } else {
+    return true;
+  }
+};
